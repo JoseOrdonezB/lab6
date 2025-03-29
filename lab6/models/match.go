a42774b1c98d225ff85e2b00ced53b7254b6a851
JS103 <131785570+JoseOrdonezB@users.noreply.github.com>
2025-03-28 21:44:07 -0600

    endpoints agregados.

--- lab6/models/match.go
+++ lab6/models/match.go
@@ -1,8 +1,12 @@
 package models
 
 type Match struct {
-    ID        uint   `json:"id" gorm:"primaryKey"`
-    HomeTeam  string `json:"homeTeam"`
-    AwayTeam  string `json:"awayTeam"`
-    MatchDate string `json:"matchDate"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	HomeTeam    string `json:"homeTeam"`
+	AwayTeam    string `json:"awayTeam"`
+	MatchDate   string `json:"matchDate"`
+	TotalGoals  int    `json:"totalGoals"`
+	YellowCards int    `json:"yellowCards"`
+	RedCards    int    `json:"redCards"`
+	ExtraTime   bool   `json:"extraTime"`
 }
